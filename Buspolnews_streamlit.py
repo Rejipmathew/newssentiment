66b2b2bba6f743d66364a33776067a75491149d6
rejipmathew <Rejipmathew@users.noreply.github.com>
2024-11-20 16:44:14 -0500

    Update Buspolnews_streamlit.py

--- Buspolnews_streamlit.py
+++ Buspolnews_streamlit.py
@@ -10,7 +10,7 @@ st.markdown("""
     <style>
         /* Main background and text styling */
         .stApp {
-            background-color: white;
+            background-color: lightblue;
             color: darkblue;
         }
 
@@ -27,7 +27,7 @@ st.markdown("""
 
         /* Table text color */
         table {
-            color: darkblue;
+            color: yellow;
         }
 
         /* Links */
